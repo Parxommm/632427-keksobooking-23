2e28dd669edbaae1c76ac67d664b9b304234e359
Parxommm <evg.parkhomenko@gmail.com>
2021-06-12 11:43:26 +0300

    Модуляция

---
+++ js/data.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+import {getRandomPositiveFloat, getRandomPositiveInteger, getRandomArrayElement, createRandomArray} from './utils.js';
+
+const TYPES_OF_HOUSING = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKIN_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUT_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const ALL_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+];
+
+const ALL_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const SIMILAR_COUNT = 10;
+
+const createAd = () => ({
+  author: {
+    avatar: `img/avatars/user0${getRandomPositiveInteger(1, 8)}.png`,
+  },
+  offer: {
+    title: 'Заголовок объявления',
+    address: `Широта: ${getRandomPositiveFloat(35.65000,  35.70000, 5)}, Долгота: ${getRandomPositiveFloat(139.70000, 139.80000, 5)}`,
+    price: getRandomPositiveInteger(10000, 1000000),
+    type: getRandomArrayElement(TYPES_OF_HOUSING),
+    rooms: getRandomPositiveInteger(1, 100),
+    guests: getRandomPositiveInteger(1, 100),
+    checkin: getRandomArrayElement(CHECKIN_TIMES),
+    checkout: getRandomArrayElement(CHECKOUT_TIMES),
+    features: createRandomArray(ALL_FEATURES, 5),
+    description: 'Описаие объявления',
+    photos: createRandomArray(ALL_PHOTOS, 3),
+  },
+  location: {
+    lat: getRandomPositiveFloat(35.65000,  35.70000, 5),
+    lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
+  },
+});
+
+
+const similarAd = new Array(SIMILAR_COUNT).fill(null).map(() => createAd());
+
+export {similarAd};
--- js/utils.js
+++ js/utils.js
@@ -5,8 +5,6 @@ function getRandomPositiveFloat (min, max, digits = 1) {
   return result.toFixed(digits);
 }
 
-getRandomPositiveFloat;
-
 function getRandomPositiveInteger (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
@@ -16,12 +14,9 @@ function getRandomPositiveInteger (min, max) {
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-
-getRandomArrayElement;
-
 // массив случайной длины из неповторяющихся значений. Взял с https://qna.habr.com/q/844269
-const createArr = ([...source], maxLength) => Array.from(
+const createRandomArray = ([...source], maxLength) => Array.from(
   { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
   () => source.splice(Math.random() * source.length | 0, 1)[0]);
 
-createArr;
+export {getRandomPositiveFloat, getRandomPositiveInteger, getRandomArrayElement, createRandomArray};
